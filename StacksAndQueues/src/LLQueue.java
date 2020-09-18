f276372250adb1cc492c89d662ed7a0783b361a9
adamseth2 <35408139+adamseth2@users.noreply.github.com>
2020-09-18 00:26:13 -0700

    2nd try is the charm

--- StacksAndQueues/src/LLQueue.java
+++ StacksAndQueues/src/LLQueue.java
@@ -13,7 +13,7 @@ public class LLQueue {
 
         // Constructor with no parameters for inner class
         public Node() {
-           
+
         }
 
         // Parametrized constructor for inner class
@@ -22,40 +22,47 @@ public class LLQueue {
             // to do: Link to next node is a type Node
         }
     }
-    
+
     private Node front;
     private Node back;
 
     public LLQueue() {
-     // to do
+        // to do
     }
-    
-    //offer(enqueue) adds the object at the back of the queue
+
+    // offer(enqueue) adds the object at the back of the queue
     public void offer(Object o) {
-     // to do
+        // to do
     }
-    
-    //poll(dequeue): retrieves and removes the head of this queue, 
-    //or returns null if this queue is empty.
+
+    // poll(dequeue): retrieves and removes the head of this queue,
+    // or returns null if this queue is empty.
     public Object poll() {
-     // to do
+        // to do
     }
-    
+
     // Returns the size of linked list by traversing the list
     public int size() {
-        // to do
+        Node currentNode = back;
+        int currentSize = 0;
+        while (currentNode != null) {
+            currentSize++;
+            currentNode = currentNode.next;
+        }
+        return currentSize;
     }
-    //peek: Retrieves, but does not remove, the head of this queue, 
-    //or returns null if this queue is empty.
+
+    // peek: Retrieves, but does not remove, the head of this queue,
+    // or returns null if this queue is empty.
     public Object peek() {
         // to do
-    } 
-    
+    }
+
     //
     public boolean isEmpty() {
         // to do
-    } 
-    
+    }
+
     // For two lists to be equal they must contain the same data items in
     // the same order. The equals method of T is used to compare data items.
     public boolean equals(Object otherObject) {
@@ -79,10 +86,10 @@ public class LLQueue {
             return true; // objects are the same
         }
     }
-    
+
     // There is no need to modify the driver
     public static void main(String[] args) {
-     // input data for testing
+        // input data for testing
         String target = "Somethings!";
         String palindrome = "a man a plan canal panama";
 
@@ -99,37 +106,35 @@ public class LLQueue {
         list.offer(object2);
         list.offer(object3);
         list.offer(object4);
-        
+
         // make sure all are added
         System.out.println("My list has " + list.size() + " nodes.");
-        
-        //testing equals
+
+        // testing equals
         LLQueue list2 = new LLQueue();
         // add 4 objects to the new linked list
-        list2.offer(object1);//t
-        list2.offer(object2);//o
-        list2.offer(object3);//m
-        list2.offer(object4);//s
+        list2.offer(object1);// t
+        list2.offer(object2);// o
+        list2.offer(object3);// m
+        list2.offer(object4);// s
         boolean isEqual2 = list.equals(list2);
         System.out.println("list2 is equal to list1? " + isEqual2);
-        
+
         // add 4 objects to our linked list in a different order
         LLQueue list3 = new LLQueue();
-        list3.offer(object3);//m
-        list3.offer(object1);//t
-        list3.offer(object2);//o
-        list3.offer(object4);//s
+        list3.offer(object3);// m
+        list3.offer(object1);// t
+        list3.offer(object2);// o
+        list3.offer(object4);// s
         boolean isEqual3 = list.equals(list3);
         System.out.println("list3 is equal to list1? " + isEqual3);
-        
+
         // testing isEmpty() and poll()
-        while(!list.isEmpty()) {
+        while (!list.isEmpty()) {
             Object temp = list.poll();
             System.out.println("Polling " + temp);
         }
-       
-    }
 
-   
+    }
 
 }
