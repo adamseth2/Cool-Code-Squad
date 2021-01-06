6c32b4a65c6e3f413aa40d39c07e7758df89445c
justindood <46207052+justindood@users.noreply.github.com>
2021-01-06 12:02:56 -0800

    Printing Words with 2000

--- WordCount/src/WordCount.java
+++ WordCount/src/WordCount.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 /*PARTICIPATION
 ADAM: Push initial commit; completed getCountMap method;took screenshot of console results
-JUSTIN: Fixed NullPointedException and fixed code not finding file because it was in the wrong place
+JUSTIN: Fixed NullPointedException and fixed FileNotFound because file was in the wrong place; Added printing of results using the 2000 occurrences method
 
 */
 public class WordCount {
@@ -13,11 +13,20 @@ public class WordCount {
 	public static final int OCCURRENCES = 2000;
 	public static void main(String[] args) throws FileNotFoundException {
 		//Fix file system probably
+		//Note to put text file in project folder and not in src folder
 		Scanner input = new Scanner(new File("mobydick.txt"));
-		// Sort values by accending order
-		System.out.println(getCountMap(input));
+		// print out only words that occur more than 2000 times
+		System.out.println("This program displays the most frequently occurring words from the book Moby-Dick: ");
+		Map<String, Integer> result = getCountMap(input);
 
+		for(Map.Entry<String, Integer> word: result.entrySet()){
+			if(word.getValue()>OCCURRENCES){
+				System.out.println(word.getKey() + " occurs " + word.getValue() + " times.");
+			}
+		}
 
+		//sort entire map for alternative solution
+		//TO DO
 
 	}
 
