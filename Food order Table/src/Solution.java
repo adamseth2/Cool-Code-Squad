435c9eb86a8519e5229cfe6231f892473730c37b
justindood <46207052+justindood@users.noreply.github.com>
2021-01-10 14:43:06 -0800

    Map to Output
    added comments for some stuff and did some of the converting Map to output of list. Left some for Jacob and wrote about what is left to do.

--- Food order Table/src/Solution.java
+++ Food order Table/src/Solution.java
@@ -1,17 +1,21 @@
 import java.util.*;
+/**
+Group Cool Code Squad
+    Adam, Jacob, Justin
 
 
+ **/
 public class Solution {
   public static void main(String[] args) {
     //Test case
-    /*    List<List<String>> orders = new ArrayList<>();
+        List<List<String>> orders = new ArrayList<>();
     String[] order1arr = {"David","3","Ceviche"};
     String[] order2arr = {"David","3","Bob"};
     List<String> order1 = Arrays.asList(order1arr);
     List<String> order2 = Arrays.asList(order2arr);
     orders.add(order1);
     orders.add(order2);
-    System.out.println(displayTable(orders));*/
+    System.out.println(displayTable(orders));
   }
   public static List<List<String>> displayTable(List<List<String>> orders) {
     Map<Integer, TreeMap<String, Integer>> tableFoodItemOrders = new TreeMap<>();
@@ -19,6 +23,7 @@ public class Solution {
     SortedSet<String> foodItems = new TreeSet<>();
 
     for (List<String> order : orders) {
+      //Must convert table number from String to Int because it is given in a list of strings
       int tableNumber = Integer.parseInt(order.get(1));
       String item = order.get(2);
       // If the table number doesn't exist, add the the number number
@@ -34,6 +39,15 @@ public class Solution {
     }
     System.out.println();
     List<List<String>> bob = new ArrayList<>();
+    //first row of the table is the list of all dishes in alphabetical order
+    bob.add(new ArrayList<>());
+    bob.get(0).add("Table");
+    bob.get(0).addAll(foodItems);
+    for(Map.Entry<Integer, TreeMap<String,Integer>> currentRow : tableFoodItemOrders.entrySet()){
+        //TO DO: out the row number at the start of each arrayList and then the number of each dish, then add arrayList to bob
+
+    }
+
     return bob;
 
     //Need to implement converting maps into List
