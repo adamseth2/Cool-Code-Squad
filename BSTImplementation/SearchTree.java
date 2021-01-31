097509b7c8dfce6e24f642cab4243052ba399d2b
adamseth2 <chhoradam@gmail.com>
2021-01-30 16:09:43 -0800

    Implemented constructor, add, contains

--- BSTImplementation/SearchTree.java
+++ BSTImplementation/SearchTree.java
@@ -2,35 +2,60 @@
 // objects of type E.  E must implement the Comparable<E>
 // interface.
 
+/*
+PARTICIPATION:
+ Adam: Implemented constructor, add, contains
+ Jacob: Kelleran:
+ Justin Zhu:
+
+ */
+
 public class SearchTree<E extends Comparable<E>> {
     private SearchTreeNode<E> overallRoot; // root of overall tree
 
     // post: constructs an empty search tree
     public SearchTree() {
 //      TO DO:
+      overallRoot = new SearchTreeNode<E>(null);
     }
 
     // post: value added to tree so as to preserve binary search tree
     public void add(E value) {
 //    	TO DO:
+      overallRoot = add(overallRoot, value);
     }
 
     // post: value added to tree so as to preserve binary search tree
     private SearchTreeNode<E> add(SearchTreeNode<E> root, E value) {
 //    	TO DO:
-        return null;
+      if (root == null) {
+        root = new SearchTreeNode<E>(value);
+      } else if (value.compareTo(root.data) > 0) {
+        root.left = add(root.left, value);
+      } else if (value.compareTo(root.data) < 0) {
+        root.right = add(root.right, value);
+      }
+      return root;
     }
 
     // post: returns true if tree contains value, returns false otherwise
     public boolean contains(E value) {
 //    	TO DO:
-    	return false;
+      return contains(overallRoot, value);
     }   
 
     // post: returns true if given tree contains value, returns false otherwise
     private boolean contains(SearchTreeNode<E> root, E value) {
 //    	TO DO:
-    	return false;
+      if (root == null) {
+        return false;
+      } else if (value.compareTo(root.data) > 0) {
+        return contains(root.left, value);
+      } else if (value.compareTo(root.data) < 0) {
+         return contains(root.right, value);
+      } else { // value.compareTo(root.data) == 0
+        return true;
+      }
     }
     
     // post: value removed from tree so as to preserve binary search tree
@@ -48,7 +73,7 @@ public class SearchTree<E extends Comparable<E>> {
  // post: return the smallest value in the binary search tree  
     private E findSmallest(SearchTreeNode<E> root) {
 //    	TO DO:
-    	return null;
+      return null;
     }
 
     // post: prints the data of the tree, one per line
